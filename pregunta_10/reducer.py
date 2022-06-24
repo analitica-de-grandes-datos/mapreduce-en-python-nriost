8df3bdd836284be76c2f91f7993901f7ad4db437
nriost <95105995+nriost@users.noreply.github.com>
2022-06-24 16:15:45 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,24 +1,26 @@
-import sys
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
-if __name__ == '__main__':
-    lista=[]
-    for line in sys.stdin:
-        letra, valor = line.split("\t")
-        valor = int(valor)
-        lista.append((letra,valor))
 
-    lista.sort(key=lambda orden: (orden[0], orden[1]))
-    curkey = None
-    for line in lista:
-        if curkey == line[0]:
-            sys.stdout.write(",{}".format(line[1]))
-        else:
-            if curkey is not None:
-                sys.stdout.write("\n{}\t{}".format(line[0],line[1]))
-                curkey = line[0]
+import sys
+
+letters_dict = {}
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        line_num, letters = line.split("\t")
+        line_num = int(line_num)
+        each_letter = letters.split(",")
+
+        for letter in each_letter:
+
+            letter = letter.strip()
+            if letter not in letters_dict.keys():
+                letters_dict[letter] = [line_num]
+
             else:
-                sys.stdout.write("{}\t{}".format(line[0],line[1]))
-                curkey = line[0] 
+                letters_dict[letter] += [line_num]
+
+    for key in sorted(letters_dict.keys()):
+        str_nums = ",".join([str(i) for i in sorted(letters_dict[key])])
+        sys.stdout.write(
+            "{}\t{}\n".format(key, str_nums))
     
